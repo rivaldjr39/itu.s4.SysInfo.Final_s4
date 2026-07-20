4744e616848eaeac407828aa760f87d438718c6b
Armel <tommyarmel2234@gmail.com>
2026-07-20 08:29:39 +0300

    configuration base de donnee

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,11 +26,11 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
+        'hostname'     => '',
         'username'     => '',
         'password'     => '',
-        'database'     => '',
-        'DBDriver'     => 'MySQLi',
+        'database'     => WRITEPATH . 'base.db',
+        'DBDriver'     => 'SQLite3',
         'DBPrefix'     => '',
         'pConnect'     => false,
         'DBDebug'      => true,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 3306,
+        'port'         => '',
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
